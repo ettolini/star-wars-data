b949c65a19d8b9456288012c113796b25a3995a2
Ettore J. Bortolin <ettorej.bortolin@gmail.com>
2021-03-10 16:28:05 -0300

    Get basic output

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            loading: false,
+            character: {}
+        }
+        this.newCharacter = this.newCharacter.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+    
+    newCharacter(n) {
+        this.setState({loading: true})
+        fetch("https://swapi.dev/api/people/" + n)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    loading: false,
+                    character: data,
+                    totalCharacters: 83
+                })
+            })
+    }
+    
+    handleChange(e) {
+        this.newCharacter(e.target.value)
+    }
+    
+    render() {
+        const { name, gender, height, mass } = this.state.character
+        let text = "loading..."
+
+        const characters = ['Luke Skywaker',
+                            'C3-PO',
+                            'R2-D2',
+                            'Darth Vader',
+                            'Leia Organa'
+                            ]
+
+        const optionComponents = characters.map((character, index) => (
+            <option
+              key={index}
+              value={`${(index + 1).toString()}/`}
+            >
+              {character}
+            </option>
+        ));
+
+        if (!this.state.loading)
+            text = name != undefined ? `Gender: ${gender} - Height: ${height} - Mass: ${mass}` : ""
+
+        return (
+            <div>
+                <h1>Star Wars Date Simulator</h1>
+                <p>{text}</p>
+                <select
+                        onChange={this.handleChange}
+                >
+                    <option value="">
+                        -- Choose a partner --
+                    </option>
+                    {optionComponents}
+                </select>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
