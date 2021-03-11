ff76b092af8611d1dd7a5926fa4fe13cf821b95f
Ettore J. Bortolin <ettorej.bortolin@gmail.com>
2021-03-11 16:16:25 -0300

    Add featured movies to character info

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
             text: ""
         }
         this.getPeople = this.getPeople.bind(this)
-        // this.getFilms = this.getFilms.bind(this)
+        this.getFilms = this.getFilms.bind(this)
         this.handleChange = this.handleChange.bind(this)
     }
 
@@ -28,32 +28,48 @@ class App extends Component {
     }
     
     getPeople(url) {
-        this.setState({text: "LOADING..."})
-
         fetch(url)
             .then(response => response.json())
             .then(data => {
-                this.setState({
-                    text: "",
-                    characters: data.results
-                })
+                this.setState({ characters: data.results })
+                this.getFilms(this.state.url.films)
             })
     }
 
-    // getFilms(url) {
-
-    // }
+    getFilms(url) {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ 
+                    movies: data.results,
+                    text: "" 
+                })
+             })
+    }
 
     handleChange(e) {
         try {
+            const movies = this.state.movies
             const character = this.state.characters[e.target.value]
-            const show = `Height: ${character.height}.
+            const featuredMovies = character.films
+            
+            let show = `Height: ${character.height}.
                         Mass: ${character.mass}.
                         Hair color: ${character.hair_color}. 
                         Skin color: ${character.skin_color}.
                         Eye color: ${character.eye_color}.
                         Birth year: ${character.birth_year}.
-                        Gender: ${character.gender}.`
+                        Gender: ${character.gender}.
+                        Featured movies:`
+
+            for (let featured of featuredMovies) {
+                for (let i = 1; i <= movies.length; i++) {
+                    if (featured[featured.length - 2] === i.toString()) {
+                        show = show.concat(" ", `"${movies[i - 1].title}".`)
+                        console.log(movies[i - 1].title)
+                    }
+                }
+            }
     
             this.setState({
                 text: show
